9e3a38a4a3f59e8a0e9092f95fea962a26a925f8
Steve <stevenlyall@gmail.com>
2016-01-06 23:02:11 -0800

    include missing Sphere source file

---
+++ src/main/java/Sphere.java
@@ -0,0 +1,14 @@
+/**
+ * Created by stevenlyall on 2016-01-06.
+ */
+public class Sphere {
+    private int radius;
+
+    public Sphere(int radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return 4 * Math.PI *Math.pow(radius, 2);
+    }
+}
